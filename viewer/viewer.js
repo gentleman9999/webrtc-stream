faf14dfea5ece0da72e00991d4e162479e900d13
gentleman9999 <gentleman99729@outlook.com>
2026-03-03 18:44:36 +0200

    updated viewer pc setting

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -2,7 +2,19 @@ const socket = io();
 const roomId = "12345";
 
 const pc = new RTCPeerConnection({
-  iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+  iceServers: [
+    { urls: "stun:stun.l.google.com:19302" },
+    {
+      urls: "turn:openrelay.metered.ca:80",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    },
+    {
+      urls: "turn:openrelay.metered.ca:443",
+      username: "openrelayproject",
+      credential: "openrelayproject"
+    }
+  ]
 });
 
 socket.emit("join-room", { roomId, role: "viewer" });
